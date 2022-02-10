54e6f4122d020033ba74ce4a1cbb34403314f356
johndward <johndward01@gmail.com>
2022-02-10 15:54:06 -0600

    Add project files.

---
+++ JamesGregoryDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JamesGregoryDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "James";
+            Console.WriteLine(firstName);
+        }
+    }
+}
